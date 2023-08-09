abe138d2824e0a8f7336fbd4e096da39ba189bc9
Prince <infor@rockblings.com>
2023-08-09 21:11:48 +0200

    added the send sms class

--- src/LaravelTwillioSms.php
+++ src/LaravelTwillioSms.php
@@ -2,6 +2,37 @@
 
 namespace creativeCrafts\LaravelTwillioSms;
 
+use Twilio\Exceptions\ConfigurationException;
+use Twilio\Exceptions\TwilioException;
+use Twilio\Rest\Api\V2010\Account\MessageInstance;
+use Twilio\Rest\Client;
+
 class LaravelTwillioSms
 {
+    protected Client $client;
+
+    /**
+     * @throws ConfigurationException
+     */
+    public function __construct()
+    {
+        /** @var string $account_sid */
+        $account_sid = config('twillio-sms.account_sid');
+
+        /** @var string $auth_token */
+        $auth_token = config('twillio-sms.auth_token');
+
+        $this->client = new Client($account_sid, $auth_token);
+    }
+
+    /**
+     * @throws TwilioException
+     */
+    public function sendSms(string $number, string $message): MessageInstance
+    {
+        return $this->client->messages->create($number, [
+            'from' => config('twillio-sms.from'),
+            'body' => $message
+        ]);
+    }
 }
---
+++ tests/LaravelTwillioSmsTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use creativeCrafts\LaravelTwillioSms\LaravelTwillioSms;
+
+
+it('can send an sms', function () {
+    $mock = Mockery::mock(LaravelTwillioSms::class);
+    $mock->shouldReceive('sendSms')->with('+2347030000000', 'Hello World')->andReturn();
+});
